5861f7685fb559de44ea55c0cdc000df3a97fec9
Iohel <joelpons9@hotmail.com>
2023-10-02 21:21:55 +0200

    Operadores i variables

---
+++ Unidad 2/script.js
@@ -0,0 +1,135 @@
+/* saluda();
+//Javacript unidad 2
+//Conexiements basics.
+tres
+tristes
+tigres
+
+function saluda(){
+    console.log("Bobble Head Perry the Platypus");
+} */
+
+// var LET cons
+/* let names = "Trax";
+var edad = 123;
+const pin = 3.14;
+patata();
+function patata(){
+    var names = "Trax";
+    var edad = 123;
+
+    console.log(names);
+    console.log(edad);
+} */
+
+//integer
+let uno = 1;
+let dos = 2;
+console.log(uno+dos);
+
+//String ""\''?
+
+let nombre = "Loan";
+let surname = 'Basler';
+let nombrecompleto = "People of this country, hear my voice, from now on you shall be ruled by the god of the new world.";
+
+let textoencomillas = "test 'poeple'";
+console.log(textoencomillas);
+
+//NUMBERS
+
+let edad = 24;
+let masedad = -12
+let años = "24";
+console.log(typeof edad);
+console.log(typeof años);
+console.log(edad-masedad);
+
+let precio = 21.99;
+console.log(typeof precio);
+let oferta = 17.98;
+console.log(precio-oferta);
+
+let aprovados = Infinity;
+let suspendindos = -Infinity;
+console.log(aprovados);
+console.log(typeof aprovados);
+console.log(aprovados-aprovados);
+
+//null
+let valor = null;
+console.log(valor);
+console.log(typeof valor);
+
+//undefined
+
+let defined
+console.log(defined);
+console.log( typeof defined);
+
+//NaN
+
+let infinity = "26"/2;
+console.log(infinity);
+
+//typeoff, delete
+//ejemplos anteriores
+console.log(typeof edad);
+delete edad;
+console.log(typeof edad);
+
+//objeto
+let persona ={
+    nombre: "xxx",
+    edad: 2
+}
+
+console.log(persona.edad);
+
+delete persona.edad;
+console.log(persona.edad);
+
+//boolean
+
+let pepe = true;
+let aass = false; 
+//false
+// null;
+// empty string;
+// undefined;
+// 0;
+// NaN;
+
+let algo;
+if (!algo) {
+    console.log("hola");    
+}else{
+    console.log("adios");
+}
+
+// + - * /
+//Operadores aritmeticos
+// 3+3=6
+// 3-3=0
+// 3*3=9
+// 3/3=1
+// 3%3=?
+
+//++a 3+1
+//--a 3-1
+
+//+= a = b+1
+//-= a = b-1
+//*= a *b
+// /= a/= b
+// %= a %=b
+
+let a = 4;
+let b = 2;
+a %= b;
+console.log(a);
+
+//cambio de signo
+// a = 5 b = -a = -5
+
+

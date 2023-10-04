8f189123b1ea95c572f4308b316acccf2f16ab34
Iohel <joelpons9@hotmail.com>
2023-10-04 20:54:29 +0200

    Ejercici Notas al final de document.

--- Unidad 3/script.js
+++ Unidad 3/script.js
@@ -11,8 +11,98 @@ if(){
 
 }
 */
-let edad = 18;
-let esMayorDeEdad = edad>=18; 
-if(esMayorDeEdad){
+//let edad = 17;
+/*
+let edad = 16;
+let esMayorDeEdad = (edad>=18); 
+ if(esMayorDeEdad){
     console.log("CERVEZA");
+}else if(edad > 12){
+    console.log("Niñato");
+}else{
+    console.log("caca");
+} 
+let resultado = esMayorDeEdad ? "CERVEZA" : "Niñato";
+console.log(resultado);
+let a = 20;
+let b = 20;
+
+resultado = (a<b) ? (b-a) : (a-b)+1;
+console.log(resultado);
+*/
+//Switch case
+//Usado quando se necesite un estructura condicional segun el valor que tenga una variable
+/* 
+let nombre = "gandalf";
+let edad;
+switch (nombre) {
+    case "frodo":
+        edad = 50;
+        break;
+    case "gandalf":
+        edad = 1238;
+        console.log("salvese quien pueda")
+        break;
+    case "aragorn":
+        edad = 532;
+        console.log("object");
+        break;
+    default:
+        edad = -1;
+        break;
 }
+console.log("Nombre: " + nombre + " Edad: " + edad); */
+
+//Bucles
+
+//Se utilizan para repertir una porcion de codigo varias veces.
+
+//while
+//repertir bloque hasta cumplir condicion
+/*let contador = 0;
+while(contador < 5){
+    console.log(contador);
+    ++contador;
+}*/
+
+
+//do-while
+//while pero pasa una vez por en medio
+/* let contador = 20;
+do{
+console.log(contador);
+contador--;
+}while(contador > 10) */
+
+//for
+//repertir numero especifico de veces
+//break(detener ejecucion)
+//continue(continua la ejecucion)
+/* for(let i = 0;i<5;i++){
+    if(i == 2){
+        i++
+        continue;
+    }
+    console.log(i);
+} */
+
+//Ejercicio Notas Modulo DWEC
+let nomEstudiant = "Joel";
+let notaExamen1 = 1;
+let notaExamen2 = 7.9;
+let notaProjecte = 8.7;
+
+let notaFinal = (notaExamen1*0.2)+(notaExamen2*0.2)+(notaProjecte*0.6);
+
+console.log("Estudiant: "+nomEstudiant);
+console.log("Nota de l'Examen 1: " + notaExamen1);
+console.log("Nota de l'Examen 2: " + notaExamen2);
+console.log("Nota del Projecte: " + notaProjecte);
+console.log("Nota Final: " + notaFinal);
+
+if(notaFinal >= 7){
+    console.log("Felicitats, " + nomEstudiant +"! Has aprovat el mòdul");
+}else{
+    console.log(nomEstudiant + ", necesites millorar la nota per aprovar el modul.");
+}
+
